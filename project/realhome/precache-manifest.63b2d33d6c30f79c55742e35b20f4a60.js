4421afd57ea745a47ceea1cc72d42daf39d72081
mai1015 <mai1015@users.noreply.github.com>
2020-01-27 22:37:40 -0500

    add proejct real home page

---
+++ project/realhome/precache-manifest.63b2d33d6c30f79c55742e35b20f4a60.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e4a3f1f4e50e6198dda3627d6b50e4",
+    "url": "/project/realhome/index.html"
+  },
+  {
+    "revision": "06167086b9270cdfe3a6",
+    "url": "/project/realhome/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "f57c1b4c19344108b375",
+    "url": "/project/realhome/static/css/main.67bac9b1.chunk.css"
+  },
+  {
+    "revision": "06167086b9270cdfe3a6",
+    "url": "/project/realhome/static/js/2.03d69ced.chunk.js"
+  },
+  {
+    "revision": "d4503a70721fea146de807f4f151ad0d",
+    "url": "/project/realhome/static/js/2.03d69ced.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f57c1b4c19344108b375",
+    "url": "/project/realhome/static/js/main.528f1f8d.chunk.js"
+  },
+  {
+    "revision": "bc81dbb733a11450a3ef",
+    "url": "/project/realhome/static/js/runtime-main.af891aba.js"
+  },
+  {
+    "revision": "350a52019a07fd305a4c51ae4b5bc1cc",
+    "url": "/project/realhome/static/media/PageNotFound.350a5201.png"
+  },
+  {
+    "revision": "ebf602aa817a919bafd7c45fce2fb393",
+    "url": "/project/realhome/static/media/bed.ebf602aa.png"
+  },
+  {
+    "revision": "ce9c29f1ff3a32baffa2171b5d25ef7d",
+    "url": "/project/realhome/static/media/chair.ce9c29f1.png"
+  },
+  {
+    "revision": "03ed1f43a4d69da34d819136821f8e01",
+    "url": "/project/realhome/static/media/chirs.03ed1f43.png"
+  },
+  {
+    "revision": "6873bb5f5c776cbb1ef1cd3b6c782feb",
+    "url": "/project/realhome/static/media/images-solid.6873bb5f.svg"
+  },
+  {
+    "revision": "e21ddc76415beabf1bf1922333e7e8e5",
+    "url": "/project/realhome/static/media/imgback-white.e21ddc76.jpg"
+  },
+  {
+    "revision": "d5eab515e05872e11d6f2565e9dc0789",
+    "url": "/project/realhome/static/media/imgback.d5eab515.jpg"
+  },
+  {
+    "revision": "6f6bc685d918ece45f6c4d4ffdeee0e1",
+    "url": "/project/realhome/static/media/parking.6f6bc685.png"
+  },
+  {
+    "revision": "6ab7085846a57f21201ff69640b1b1d1",
+    "url": "/project/realhome/static/media/rachel.6ab70858.png"
+  },
+  {
+    "revision": "f56ef1a234b4e47e6451e69e95b1b994",
+    "url": "/project/realhome/static/media/remax.f56ef1a2.png"
+  },
+  {
+    "revision": "0a1ad619ed11324085cf6fc5cf8dea7b",
+    "url": "/project/realhome/static/media/remax_one.0a1ad619.png"
+  }
+]);
